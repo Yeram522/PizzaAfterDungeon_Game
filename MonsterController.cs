a56208a1900363450b7fa2065cc32fff41a6e0b3
Yeram <63442636+Yeram522@users.noreply.github.com>
2021-02-01 00:04:50 +0900

    클래스 버거킹 및 클래스 monsterController
    버거킹 세부 스킬 및 행동패턴 구현(paze1 완료/paze2 의 attack은 아직 미구현)
    check_condition() 함수 작성
    monster class 수정> 기존 monstercontroller에서 instantiate했는데, 이젠 new로 클래스 생성시 바로 자기 스스로를 instantiate하도록 만듦.
    class monster의 list를 만듦. 이를 이용해 data관리 예정.

--- MonsterController.cs
+++ MonsterController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 static class Costants
 {
     public const float ACCESS = 0.5f;
-
+    public const int STD = 0;
+    public const int TRACE = 1;
+    public const int ATTACK = 2;
+    public const int DIE = 3;
+    public const int ATTACK_STD = 4;
 }
 
 public class Monster
 {
+    public static List<Monster> monsters = new List<Monster>();
+    public GameObject player = GameObject.FindGameObjectWithTag("Player");
     public int id;
     public Animator animator;
     public GameObject me;
@@ -17,25 +23,34 @@ public class Monster
     public SpriteRenderer sRenderer;
     public float hp;  //체력
     public float speed; //속도
+    public int condition; //0=std 1=trace 2=attack 3=die 4=attack_std
     public bool trace_trigger;
     public float eyesight;
+    public float std_time = 0;
+   
 
     //player와 moster 사이의 거리를 float형식으로 반환한다.
-    public float cal_distance(GameObject player)
+    public float cal_distance()
     {
         float playerpos = player.transform.position.x; //참조
         float monsterpose = me.transform.position.x;
 
-        eyesight = playerpos - monsterpose;
-        return Mathf.Abs(eyesight);
+        float distance = playerpos - monsterpose;
+        return Mathf.Abs(distance);
     }
 
+    //information update
+    public void update_infor()
+    {
+
+    }
     public virtual void check_condition()
     {
         //몬스터마다 컨디션에 따른 행동 기준이 다르기 떄문에 재정의한다.
     }
+
     //플레이어의 방향에 따라 몬스터가 감지하고 쫓아간다.
-    public void TRACE(GameObject player)
+    public void Trace()
     {
         float playerpos = player.transform.position.x;
         float monsterpose = me.transform.position.x;
@@ -46,7 +61,8 @@ public class Monster
             //Debug.Log("outtrace");
             return;
         }
-        //animator.SetBool("isFound", true); //달리는 모션
+        //player가 특정범위 안에 있으면 대기상태이고 없으면 추적한다.
+
 
         if (monsterpose < playerpos)
         {
@@ -65,19 +81,20 @@ public class Monster
     }
 
     //자식class에서 재정의 가능하다.
-    public virtual void ATTACK(GameObject player)
+    public virtual void Attack()
     {
+        if (condition != Costants.ATTACK)
+            return;
         //공격에 따라서, 데미지주는 공격력등이 차이가 이씀
     }
 
-    //대기상태
-    public void STD()
+    //대기상태 
+    public virtual void Std()
     {
-        //가만히 있다가 일정시간 움직인다.
     }
 
     //몬스터의 개체를 삭제한다.
-    void Death()
+    public void Death()
     {
         animator.SetBool("isDeath", true);//죽음 애니메이션 실행
 
@@ -87,6 +104,8 @@ public class Monster
 
 public class boss_burgerking : Monster
 {
+    public new float std_time = 5; //하이딩
+    private int count_spon =4; //미니 버거 소환하는 수 ++2 증감식으로 늘림/보호
     public boss_burgerking()
     {
         //instance 초기화
@@ -97,82 +116,136 @@ public class boss_burgerking : Monster
         this.sRenderer = this.me.GetComponent<SpriteRenderer>();//Flip
         this.hp = 100;
         this.speed = 1.5f;
+        this.condition = Costants.STD; //초기는 std
+        this.eyesight = 0.7f; //test하면서 바꾸기
     }
-    
-    //player가 일정 거리에 접근하면 true를 반환한다.
-    public bool isAccess(GameObject player) 
+
+    //계속 update함수에 있어야함.
+    public override void check_condition()
     {
-        if (Costants.ACCESS < cal_distance(player)) //값은 테스트하면서 변경하기
+        //base.check_condition();
+        switch (condition)
+        {
+            case Costants.STD:
+                Std(); //condition이 바뀌지 않는한 계속 이 함수가 실행될것임.
+                break;
+            case Costants.TRACE:
+                //paze2에서 필요
+                Trace();
+                break;
+            case Costants.ATTACK:
+                Attack(); //세부 어택 미구현
+                break;
+            case Costants.DIE:
+                Death();
+                //배열에서도 삭제되야함!! 아직 미구현
+                break;
+            case Costants.ATTACK_STD:
+                //필용없음 곧 지울거임
+                
+                break;
+        }
+
+        
+    }
+
+    public override void Std()
+    {
+        base.Std();
+        //paze2. 이걸 구별하는 방법은. ent_pa_2가 true가 되면 바뀐다.
+        if (animator.GetBool("ent_pa_2") == true)
+        {
+            animator.SetBool("isFound", false);
+            condition = 4;
+            animator.SetBool("isFound", true); //달리는 모션 활성화
+
+            return; //밑에 실행 안하고 빠져나가기, 스위치 문으로 바꿀예정.
+        }
+
+        //paze1 std 랑 paze2 std가 다르다.
+        //가만히 있다가 일정시간 움직인다.
+        //paze1에서는 일정시간마다 마법봉으로 버거 소환함
+        if(isAccess()!=true)
+        {
+            
+            return;
+        }
+
+        //countdown으로 설정된 time값이 0이 될때마다 실행한다. 실행된후 타임을 다시 셋팅
+        if (Mathf.Round(std_time) <= 0)
+        {
+            
+            for(int i =0; i< count_spon; i++)
+            {
+                animator.SetBool("isAtk_2", true);//접근된것이 확인되면, 마법봉으로 내려치고 병사를 소환한다.
+                new normal_burgersoldier();
+                animator.SetBool("isAtk_2", false);//다시 false로 바꾸여 대기상태로 전환!!
+            }
+            std_time += 10; //총3번소환하는데 갈수롥 버거가 많아지므로 시간간격 늘이기?
+            count_spon += 2; //다음 시간대 오면 8,10으로 소환!
+
+        }
+
+        std_time -= Time.deltaTime;
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+    }
+    //player가 일정 거리에 접근하면 true를 반환한다.
+    public bool isAccess() 
+    {
+        if (Costants.ACCESS < cal_distance()) //값은 테스트하면서 변경하기
             return false;
         Debug.Log("player_monster_ACCESS");
-        //trace_trigger = true;
+        animator.SetBool("isAtk_1", true); //처음 시작하는 모션 burgerking_atk_21
+        //paze1이기 때문에 컨디션은 그대로.
         return true;
     }
+
 }
 
 public class normal_burgersoldier: Monster
 {
+    //new normal_burgersoldier을 하면 생성자에서 자동으로 prefab clone을 복제한다.
     public normal_burgersoldier()
     {
-        this.me = GameObject.Find("Burger_soldier"); //오브젝트 할당
+        this.me = 
+            MonsterController.Instantiate(GameObject.Find("Burgersoldier"), MonsterController.spon_position(), me.transform.rotation);
         this.rgd = this.me.GetComponent<Rigidbody2D>(); //rigidbody
         this.sRenderer = this.me.GetComponent<SpriteRenderer>();//Flip
         this.hp = 100;
         this.speed = 1.5f;
-        this.eyesight = 1.2f;
+        this.eyesight = 0.4f; //test하면서 바꾸기
+        Monster.monsters.Add(this); //자기 자신을 배열에 넣기
     }
     //충돌시 데미지 플레이어와 일정거리 가까어졌을 때 내리치는 모션
-    public override void ATTACK(GameObject player)
+    public override void Attack()
     {
         //가시거리 안에 들면 내리치는 모션을 한다.
-        if (cal_distance(player) < eyesight)
+        if (cal_distance() < eyesight)
             return; // 내리치는 모션, 충돌감지되면 player는 데미지입음
-
     }
+   
 }
 
+//몬스터클래스의 메서드들을 사용한다.
 public class MonsterController : MonoBehaviour
-{ 
-    public  List<Monster> monsters = new List<Monster>();
+{  
     public GameObject Burgerking, Burgersoldier;
     public GameObject player;
     public Vector2 playerPos; //플레이어의 실시간 위치를 반영한다(update에서)
-                              //몬스터 개수에 따라 크기는 달라질 수 있음
-
-    //몬스터를 생성한다.
-    
-
 
     //몬스터 스폰 위치를 결정한다.
-    public Vector2 spon_position()
+    public static Vector2 spon_position()
     {
         Vector2 rand_pos;
-        rand_pos.x = 1.2f;
-        rand_pos.y = 1.3f;
+        rand_pos.x = 68.13f;
+        rand_pos.y = -63.46f;
         //rand함수로 아무데나 스폰
         return rand_pos;
     }
-
-    //monobehavior을 이용하여 몬스터를 생성한다.
-    void spon_monster(Monster monster)
-    {
-        Vector2 rand_pos = spon_position();
-        //몬스터의 생성위치/방향을 정한다..(monster class에서 정의된 메서드 사용)
-
-        //생성하고싶은 몬스터 타입을 받아와서 위치에 생성
-        GameObject instance = Instantiate(monster.me, rand_pos, monster.me.transform.rotation) as GameObject;
-
-        //생성여부를 확인한다.
-        if (instance != null)
-            Debug.Log(monster + "캐스팅 성공");
-        else
-            Debug.Log(monster + "캐스팅 실패");
-
-        //monster data에 넣기 계속 업데이트 해줘야함
-        //생성된 monster의 정보를 list에 업데이트 한다.
-        monsters.Add(new Monster() { me = monster.me, hp = monster.hp });
-    }
-
   
 
     //몬스터의 체력정보와 condition을 업데이트한다.
@@ -180,15 +253,15 @@ public class MonsterController : MonoBehaviour
     {
         //for을 이용해 mosters리스트를 탐색한다
         //그중 hp가 0이된 객체는 destroy monster()실행하며 animation보여주고 리스트에서 삭제한다.
-        foreach(var item in monsters)
+        foreach(var item in Monster.monsters)
         {
-            //2021/01.31 패턴일치사용한 클래스 분석?
+            
         }
     }
 
     void Start()
     {
-
+      
     }
 
     // Update is called once per frame
